8782a8c765d0763d3c1ad70ecca89b35297ac869
pinalikefruit <pinajoma0@gmail.com>
2024-04-05 09:58:54 -0300

    [ADD] sherlock implementation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Pending
-- add data from the terminal and  add the option for commit hash 
-- Add code. and ping the file that in scope
+- ping the file that in scope
 - Add complexity
 - Print graph fron slither  
+- Diff for private and competitive
--- run_script.sh
+++ run_script.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Define the git repository URL
-git_repo_url="https://github.com/Cyfrin/5-t-swap-audit"
-commit_hash=""
+git_repo_url="https://github.com/code-423n4/2024-04-panoptic.git"
+# commit_hash=""
 
 
 # Extract the repository name from the URL
 repo_name=$(basename -s .git "$git_repo_url")
-
+repo_name_without_date="${repo_name##*-}"
 # Define some values 
-export TITLE=$repo_name
-export LOGO="https://github.com/Cyfrin/5-t-swap-audit/raw/main/images/t-swap-youtube-dimensions.png"
-export PLATFORM="CodeHawks"
-export TOTAL_PRIZE=0
-export START_DATE="2024-03-30"
-export END_DATE="2024-04-04"
+export TITLE=$repo_name_without_date
+export LOGO="https://code4rena.com/_next/image?url=https%3A%2F%2Fstorage.googleapis.com%2Fcdn-c4-uploads-v0%2Fuploads%2FXcKzZ6eSLH9.0&w=256&q=75"
 
+export TOTAL_PRIZE=60800
+export START_DATE="2024-03-15"
+export END_DATE="2024-04-05"
+export PLATFORM="Code4rena"
+
+# Code4rena Sherlock CodeHawks Immunifi 
 
 
 # Define the directory name to clone the repoty into
@@ -31,13 +33,27 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-#Copy notes 
+
+
+if [ "$PLATFORM" == "Sherlock" ]; then
+    # Find the non-hidden directory
+    non_hidden_dir=$(find $repo_dir -maxdepth 1 -type d ! -name ".*" ! -name "$(basename $repo_dir)" -exec basename {} \;)
+    # Update repo_dir to include the non-hidden directory
+    if [ ! -z "$non_hidden_dir" ]; then
+        repo_dir="$repo_dir/$non_hidden_dir"
+    else
+        echo "No non-hidden directory found. Continuing with the original repo_dir."
+    fi
+fi
+
+#Copy notes
 
 mkdir $repo_dir/audit-data
 cp ./.notes.md $repo_dir/audit-data
-cp ./report.md $repo_dir/audit-data
+# cp ./report.md $repo_dir/audit-data
 cp ./finding-layout.md $repo_dir/audit-data
 cp ./finding-layout.md $repo_dir/audit-data/findings.md
+cp ./slither.config.json $repo_dir
 
 # Change directory to the repository
 cd $repo_dir || exit
@@ -50,50 +66,72 @@ cd $repo_dir || exit
 
 
 # Attempt to build the project with forge
-make
+# forge build
 
-# Check if forge build was successful
+# # Check if forge build was successful
 if [ $? -ne 0 ]; then
     echo "forge build failed, but continuing with cloc analysis..."
 fi
 
+code .
 # Run Coverage
-forge coverage
+# forge coverage
 
-# Run Aderyn 
-aderyn .
+# # Run Aderyn 
+# aderyn .
 
-slither . 
+# slither . --config-file slither.config.json 
 
-cd ../../setup-security-review
+if [ "$PLATFORM" == "Sherlock" ]; then
+    cd ../../../setup-security-review
+else
+    cd ../../setup-security-review
+fi
 
 # # Run cloc and save the output to a temporary file
+# Check if scope.txt exists
+if [ -f "$repo_dir/scope.txt" ]; then
+    echo "scope.txt found, proceeding with cloc analysis."
 
-# Ask the user if scope.txt exists
-read -p "Does scope.txt exist in the repository? (y/n) " answer
+    # Initialize an empty string to hold the paths
+    paths=""
 
-# Convert the answer to lowercase
-answer=$(echo "$answer" | tr '[:upper:]' '[:lower:]')
+    # Concatenate each line from scope.txt to the paths variable
+    while IFS= read -r line; do
+        paths="$paths $repo_dir/$line"
+    done < "$repo_dir/scope.txt"
 
-# If the user answers 'y', proceed with the cloc analysis based on scope.txt
-if [[ $answer == "y" ]]; then
-    if [ -f "$repo_dir/scope.txt" ]; then
-        # Initialize an empty string to hold the paths
+    # Run cloc on all the paths
+    cloc $paths --by-file --csv --out=temp_results.csv
+else
+    echo "scope.txt not found, generating a new scope file."
+
+    if [ "$PLATFORM" == "Sherlock" ]; then
+        tree $repo_dir/contracts/ | sed 's/└/#/g; s/──/--/g; s/├/#/g; s/│ /|/g; s/│/|/g' > $repo_dir/scope.txt
+    else
+        tree $repo_dir/src/ | sed 's/└/#/g; s/──/--/g; s/├/#/g; s/│ /|/g; s/│/|/g' > $repo_dir/scope.txt
+    fi
+    # Generate scope.txt
+    
+
+    # Display the generated scope.txt and ask for confirmation
+    echo "scope.txt has been generated with the following content:"
+    cat $repo_dir/scope.txt
+    read -p "Proceed with the cloc analysis based on this scope? (y/n) " confirm
+
+    # Convert the confirm answer to lowercase
+    confirm=$(echo "$confirm" | tr '[:upper:]' '[:lower:]')
+
+    # If the user confirms, run cloc analysis
+    if [[ $confirm == "y" ]]; then
         paths=""
-
-        # Concatenate each line from scope.txt to the paths variable
         while IFS= read -r line; do
             paths="$paths $repo_dir/$line"
         done < "$repo_dir/scope.txt"
-
-        # Run cloc on all the paths
         cloc $paths --by-file --csv --out=temp_results.csv
     else
-        echo "scope.txt not found in the repository."
+        echo "cloc analysis canceled."
     fi
-else
-    # If the user answers 'n', run cloc on the src directory as before
-    cloc $repo_dir/src/ --by-file --csv --out=temp_results.csv
 fi
 
 # Call the Python script to process the results
